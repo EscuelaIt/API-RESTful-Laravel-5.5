cdc039914eae99500f97aad721bfcb7c7de0155b
JuanDMeGon <juandmegon@gmail.com>
2018-01-16 15:39:28 -0500

    Clase1: Introducción y Preparación del Proyecto

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Transaction::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::apiResource('usuarios', 'UserController');
+Route::apiResource('products', 'ProductController');
+Route::apiResource('transactions', 'TransactionController');
+Route::apiResource('categories', 'CategoryController');
